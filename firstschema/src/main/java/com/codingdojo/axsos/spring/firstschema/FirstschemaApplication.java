42ffd011fadc6f023e28425a1fd2d6fc34a67368
aseeladel1 <aseeladel100@gmail.com>
2022-11-07 02:02:15 +0200

    BookApi

---
+++ firstschema/src/main/java/com/codingdojo/axsos/spring/firstschema/FirstschemaApplication.java
@@ -0,0 +1,13 @@
+package com.codingdojo.axsos.spring.firstschema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstschemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstschemaApplication.class, args);
+	}
+
+}
---
+++ firstschema/src/test/java/com/codingdojo/axsos/spring/firstschema/FirstschemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.axsos.spring.firstschema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstschemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
